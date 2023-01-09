7b3ef4cdd45c6ce814431b2b422c42c98f576620
softyak <rsat.memis@gmail.com>
2023-01-09 22:30:41 +0300

    some changes

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Resat Memis';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Example blog';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Blog site"
         />
         <meta
           property="og:image"
