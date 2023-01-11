b0ac85f28e19be7c6f2ed2db5082fdb019e3c42e
softyak <rsat.memis@gmail.com>
2023-01-11 23:43:34 +0300

    implementing the static props

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,6 +20,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>I am a Software Developer who is curious about many parts of Software Development Job as Full Stack. I am currently working on Web Development area as Junior Front End Developer. Back End development, Developing Mobile Applications, DevOps operations and some other parts of software are also my interesting areas. However, Front End Development is enjoyable area for me and I prefer to focus on this area. Briefly, you can know me as an Eager to Learn, Curious and fast learner Software Developer.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
