0067978493d37041356a7dba0887fe6ebe351052
softyak <rsat.memis@gmail.com>
2023-01-13 00:37:14 +0300

    fix texts

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Resat Memis';
+const name = 'Reşat Memiş';
 export const siteTitle = 'Example blog';
 
 export default function Layout({ children, home }) {
